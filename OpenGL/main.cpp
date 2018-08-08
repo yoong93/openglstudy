1cf0dbeee0c23eb876933308f817c258c075da80
yoong93 <yong_hyun_kwon@brown.edu>
2018-08-08 16:12:33 +0900

    Initial Commit

---
+++ OpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL
+//
+//  Created by Yong Hyun Kwon on 8/8/18.
+//  Copyright © 2018 Yong Hyun Kwon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
